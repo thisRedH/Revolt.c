7c01fdf6743fbe7c7b7e06dc783bfa3f8bfa8de0
thisRedH <thisRedH@users.noreply.github.com>
2024-06-30 19:57:31 +0200

    fix: Add better defines

--- include/revolt/common.h
+++ include/revolt/common.h
@@ -3,10 +3,40 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#else
 #include <pstdint/pstdint.h>
+#endif
 
-#ifdef __cplusplus
-extern "C" {
+/* IEEE Std. 1003.1-1990 */
+#if defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE == 1L
+#undef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 199009L
+#endif
+
+/* IEEE Std. 1003.2-1992 */
+#if defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE == 2L
+#undef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 199209L
+#endif
+
+/* X/Open */
+#ifdef _XOPEN_SOURCE
+#if (_XOPEN_SOURCE - 0L >= 700L) && (!defined(_POSIX_C_SOURCE) || _POSIX_C_SOURCE - 0L < 200809L)
+    #undef _POSIX_C_SOURCE
+    #define _POSIX_C_SOURCE 200809L
+#elif (_XOPEN_SOURCE - 0L >= 600L) && (!defined(_POSIX_C_SOURCE) || _POSIX_C_SOURCE - 0L < 200112L)
+    #undef _POSIX_C_SOURCE
+    #define _POSIX_C_SOURCE 200112L
+#elif (_XOPEN_SOURCE - 0L >= 500L) && (!defined(_POSIX_C_SOURCE) || _POSIX_C_SOURCE - 0L < 199506L)
+    #undef _POSIX_C_SOURCE
+    #define _POSIX_C_SOURCE 199506L
+#endif
+#endif
+
+#if defined(_POSIX_SOURCE) && !defined(_POSIX_C_SOURCE)
+#define _POSIX_C_SOURCE 198808L
 #endif
 
 /* Platform detection */
@@ -14,9 +44,10 @@ extern "C" {
     #define RVLTC_OS_WIN 1
 #elif defined(linux) || defined(__linux__) || defined(__linux)
     #define RVLTC_OS_LINUX 1
+    #define RVLTC_OS_UNIX 1
 #elif defined(__APPLE__) && defined(__MACH__)
     #define RVLTC_OS_APPLE 1
-    #warning "APPLE is not supported"
+    #warning "APPLE is not officially supported"
 #elif defined(unix) || defined(__unix__) || defined(__unix)
     #define RVLTC_OS_UNIX 1
 #else
@@ -37,9 +68,15 @@ extern "C" {
     #warning "Could not detect compiler"
 #endif
 
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
+    #define RVLTC_RESTRICT                      restrict
+#else
+    #define RVLTC_RESTRICT
+#endif
+
 #ifndef RVLTC_INLINE
 #if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
-    #define RVLTC_INLINE             inline
+    #define RVLTC_INLINE                        inline
 #else
     #if defined(RVLTC_COMP_MSVC)
         #define RVLTC_INLINE                    __inline
@@ -77,19 +114,37 @@ extern "C" {
         #endif
     #endif
 #elif defined(RVLTC_OS_UNIX) && defined(RVLTC_COMP_GCC_CLANG)
-    #define RVLTC_EXPORT                        __attribute__((visibility("default")))^
+    #define RVLTC_EXPORT                        __attribute__((visibility("default")))
 #else
     #define RVLTC_EXPORT
 #endif
 #endif /* RVLTC_EXPORT */
 
-#ifdef RVLTC_OS_WIN
-    #define snprintf _snprintf
+#ifdef __cplusplus
+extern "C" {
 #endif
 
 #define RVLTC_MAX(x, y)                         ((x) > (y) ? (x) : (y))
 #define RVLTC_MIN(x, y)                         ((x) < (y) ? (x) : (y))
 #define RVLTC_CLAMP(x, min, max)                RVLTC_MIN(RVLTC_MAX((x), (min)), (max))
+
+#if defined(RVLTC_OS_UNIX)
+    #if _POSIX_C_SOURCE >= 199309L
+        #include <time.h>
+        #define revolt_sleep(ms) {              \
+            struct timespec ts;                 \
+            ts.tv_sec = (ms) / 1000;            \
+            ts.tv_nsec = ((ms) % 1000) * 1000000; \
+            nanosleep(&ts, NULL);               \
+        }
+    #else
+        #include <unistd.h>
+        #define revolt_sleep(ms)                (void)usleep((unsigned int)((ms) * 1000))
+    #endif
+#elif defined(RVLTC_OS_WIN)
+    #include <windows.h>
+    #define revolt_sleep(ms)                    Sleep((DWORD)(ms))
+#endif
 
 typedef enum _revolt_bool {
     revolt_false = 0,
